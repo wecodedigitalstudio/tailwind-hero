8a9be1f25b7608c047dc7307d4d69be9551fd974
Giacomo Corallini <corallinigiacomo89@gmail.com>
2019-09-16 18:40:41 +0200

    font size theming + col

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      primary: '#212121',
+      secondary: '#14FFEC',
+      white: '#fff'
+    },
+    fontFamily: {
+      'primary': ['Montserrat'],
+      'secondary': ['Menlo'],
+    },
+    fontSize: {
+      'h1': '55px',
+      'h5': '30px'
+    } 
   },
   variants: {},
   plugins: [],
