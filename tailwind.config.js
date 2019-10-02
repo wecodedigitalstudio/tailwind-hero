437495945aff7f50ce46bbc32a833c37e565b981
Giacomo Corallini <corallinigiacomo89@gmail.com>
2019-10-02 18:00:14 +0200

    creato container in basso a dx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   theme: {
-    colors: {
-      transparent: 'transparent',
-      primary: '#212121',
-      secondary: '#14FFEC',
-      white: '#fff'
-    },
     fontFamily: {
       'primary': ['Montserrat'],
       'secondary': ['Menlo'],
